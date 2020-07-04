053b764b91c3e85a7d4f8a147b68e91bcc043dd0
varunk2 <varunk22110@gmail.com>
2020-07-04 15:24:02 +0530

    Implmented the PlayerController script and added a rigid body to the character.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator playerAnimator;
+
+    private void Awake()
+    {
+        Debug.Log("Player Controller Awake.");
+    }
+
+    //Collision Detection
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("Collision " + collision.gameObject.name);
+    //}
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        playerAnimator.SetFloat("Speed", speed);
+    }
+}
