48d175cd3c24a69a0a59c9d8c42fb9024a4652da
varunk2 <varunk22110@gmail.com>
2020-07-04 23:17:14 +0530

    Implemented crouch animation. Resolved crouch game loop. Implemented resizing of BoxCollider in crouch position and in idle position.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,37 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator playerAnimator;
-
-    private void Awake()
-    {
-        Debug.Log("Player Controller Awake.");
-    }
-
-    //Collision Detection
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    Debug.Log("Collision " + collision.gameObject.name);
-    //}
+    public BoxCollider2D playerCollider;     // Creating a BoxCollider2D variable
+    private float p_sizeX, p_sizeY;          // Variables for storing X and Y coordinates of Box Collider.
 
     // Start is called before the first frame update
-    /*void Start()
+    void Start()
     {
-        
-    }*/
+
+        // Initializing and assigning variables
+        playerCollider = GetComponent<BoxCollider2D>();
+        p_sizeX = playerCollider.size.x;
+        p_sizeY = playerCollider.size.y;
+    }
 
     // Update is called once per frame
     void Update()
-    {
+    {        
+        // Run animation code.
         float speed = Input.GetAxisRaw("Horizontal");
         playerAnimator.SetFloat("Speed", Mathf.Abs(speed)); // Mathf.Abs() is used for converting negative values to positive values.
 
         Vector3 scale = transform.localScale;
-
-        if (speed < 0) scale.x = -1f * Mathf.Abs(scale.x);
-        else if (speed > 0) scale.x = Mathf.Abs(scale.x);
-
-        //scale.x = (speed > 0) ? -1f * Mathf.Abs(scale.x) : Mathf.Abs(scale.x);
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
 
         transform.localScale = scale;
+
+        // Crouch animation code.
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            playerAnimator.SetBool("isCrouch", true);
+            playerCollider.size = new Vector2(x: p_sizeX, 2.24f);   // Resizing BoxCollider in crouch
+            Debug.Log("Current BoxCollider2D Size while crouching: " + GetComponent<BoxCollider2D>().size);
+        }
+        else {
+            playerAnimator.SetBool("isCrouch", false);
+            playerCollider.size = new Vector2(x: p_sizeX, y: p_sizeY);   // Resetting the BoxCollider in idle
+            Debug.Log("Current BoxCollider2D Size while standing: " + GetComponent<BoxCollider2D>().size);
+        }
+
+
     }
 }
