4d35db8fe74b613a246343cbbb3bc387563b9b88
varunk2 <varunk22110@gmail.com>
2020-07-09 18:44:25 +0530

    Implemented Death condition and restart level.

---
+++ Assets/Scripts/DeathController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DeathController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>()) {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
