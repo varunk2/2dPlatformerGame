7d4793a74c693ac90806bef3b8ddb2cd78ca23a3
varunk2 <varunk22110@gmail.com>
2020-07-10 23:20:03 +0530

    Applied key collectible and increasing the player score on collecting a key

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
+    public ScoreController scoreController;
+
     private Animator playerAnimator;
     private BoxCollider2D playerCollider;    // Creating a BoxCollider2D variable
     private SpriteRenderer spriteRender;
@@ -13,7 +17,7 @@ public class PlayerController : MonoBehaviour
     private float playerColliderYAxis = 2.24f;
     public float jump;
     private float speed = 5.0f;//, jump = 6.0f;
-
+    private bool onGround = true;    
 
     private void Awake()
     {
@@ -21,6 +25,8 @@ public class PlayerController : MonoBehaviour
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         spriteRender = gameObject.GetComponent<SpriteRenderer>();
         playerCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        Debug.Log("Layer of Ellen: " + spriteRender.sortingOrder);
     }
 
     void Start()
@@ -28,6 +34,12 @@ public class PlayerController : MonoBehaviour
         // Initializing and assigning variables
         playerColliderSizeX = playerCollider.size.x;
         playerColliderSizeY = playerCollider.size.y;
+    }
+
+    public void PickUpKey()
+    {
+        Debug.Log("Player picked up the key.");
+        scoreController.IncreseScore(10);
     }
 
     void Update()
@@ -39,6 +51,7 @@ public class PlayerController : MonoBehaviour
         playerRun(h_Movement, vertical);
         playerCrouch();
         playerJump(vertical);
+        //playerJump();
     }    
 
     private void playerRun(float h_Movement, float vertical)
@@ -79,6 +92,41 @@ public class PlayerController : MonoBehaviour
     {
         if (vertical > 0)
         {
+            //rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+            playerAnimator.SetBool("Jump", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("Jump", false);
+        }
+        jumpForce();
+        
+        //playerAnimator.SetBool("Jump", ((vertical > 0) ? true : false));
+    }
+
+    private void jumpForce() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onGround)
+            {
+                rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+                Debug.Log("Player jumped.");
+
+            }
+            //playerAnimator.SetBool("Jump", true);
+        }
+    }
+
+    private void MoveCharacter(float h_Movement, float vertical)
+    {
+        // Mover player horizontally
+        Vector3 position = transform.position;
+        position.x += h_Movement * speed * Time.deltaTime;
+        transform.position = position;
+
+        // Move player vertically
+        /*if (vertical > 0)
+        {
             rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
             playerAnimator.SetBool("Jump", true);
         }
@@ -86,13 +134,29 @@ public class PlayerController : MonoBehaviour
         {
             playerAnimator.SetBool("Jump", false);
         }
+
+        playerAnimator.SetBool("Jump", ((vertical > 0) ? true : false));*/
     }
 
-    private void MoveCharacter(float h_Movement, float vertical)
+    /*private void OnCollisionEnter2D(Collision2D otherObject)
     {
-        // Mover character horizontally
-        Vector3 position = transform.position;
-        position.x += h_Movement * speed * Time.deltaTime;
-        transform.position = position;
-    }
+        TilemapRenderer otherObjectTilemapRender = otherObject.gameObject.GetComponent<TilemapRenderer>();
+
+        if(otherObjectTilemapRender.sortingOrder != spriteRender.sortingOrder)
+        {
+            onGround = true;
+            Debug.Log("Ground: " + onGround);
+        }
+    }*/
+
+    /*private void OnCollisionExit2D(Collision2D otherObject)
+    {
+        TilemapRenderer otherObjectTilemapRender = otherObject.gameObject.GetComponent<TilemapRenderer>();
+
+        if (otherObjectTilemapRender.sortingOrder != spriteRender.sortingOrder)
+        {
+            onGround = false;
+            Debug.Log("Ground: " + onGround);
+        }
+    }*/
 }
